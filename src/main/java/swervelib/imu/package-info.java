8d25d78713915ebc898cefd0d6a14ab164b231d9
Zachary Barton <122578205+ZacAttackShack@users.noreply.github.com>
2024-02-05 20:40:37 -0500

    Added A lot of stuff from Dow Swerve Robot

---
+++ src/main/java/swervelib/imu/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * IMUs used for controlling the robot heading. All implement
+ * {@link swervelib.imu.SwerveIMU}.
+ */
+package swervelib.imu;
---
+++ src/main/java/swervelib/motors/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Swerve motor controller wrappers which implement
+ * {@link swervelib.motors.SwerveMotor}.
+ */
+package swervelib.motors;
